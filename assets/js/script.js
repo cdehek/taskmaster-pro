da6115575bb9cd47bbe42481b9b7e36ea8749ee6
Christian <christian.dehek@gmail.com>
2020-08-26 18:25:57 -0600

    touch-ups

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,7 +79,7 @@ $(".list-group").on("click", "p", function() {
 });
 
 $(".list-group").on("blur", "textarea", function(){
-  // get the textares current value/text
+  // get the textareas current value/text
   var text = $(this)
     .val()
     .trim();
@@ -138,29 +138,20 @@ $(".list-group").on("click", "span", function() {
 //value of due date was changed
 $(".list-group").on("change", "input[type='text']", function() {
   //get current text
-  var date = $(this)
-    .val()
-    .trim();
+  var date = $(this).val();
   
   //get the parent ul's id attribute
-  var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
+  var status = $(this).closest(".list-group").attr("id").replace("list-", "");
 
   // get the task's position in the list of other li elements
-  var index = $(this)
-    .closest(".list-group-item")
-    .index();
+  var index = $(this).closest(".list-group-item").index();
 
   // update task in array and re-save to localStorage
   tasks[status][index].date = date;
   saveTasks();
 
   //recreate span elenemt with boostrap classes
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(date);
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(date);
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
@@ -175,15 +166,19 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass("dropover")
     console.log("activate", this);
   },
   deactivate: function(event) {
+    $(this).removeClass("dropover")
     console.log("deactivate", this)
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active")
     console.log("over", event.target);
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active")
     console.log("out", event.target);
   },
   update: function(event) {
@@ -270,6 +265,13 @@ $("#task-form-modal .btn-primary").click(function() {
   }
 });
 
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+    console.log(taskEl);
+  });
+}, 1800000);
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
   for (var key in tasks) {
@@ -281,5 +283,4 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
 
